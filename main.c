4da31212d2c520730432babe13562a0e7322b7c0
Petre Ioan Iulian <ioan_iulian.petre@stud.fiir.upb.ro>
2025-04-14 22:29:08 +0300

    Se roteste paraplegic

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include "raylib.h"
 
+typedef enum Direction {
+    IDLE,
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+} DIRECTION;
+
 int velocity_x = 5;
 int velocity_y = 5;
 
-void drawPacMan(Texture2D player, int x, int y);
-void updatePosition(int *x, int *y);
+void drawPacMan(Texture2D player, int x, int y, DIRECTION direction);
+void updatePosition(int *x, int *y, DIRECTION *direction);
 
 int main(void) {
     const int screenWidth = 800;
@@ -19,29 +27,31 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "PacMan");
     SetTargetFPS(frameRate);
 
+    DIRECTION direction = IDLE;
+
     Texture2D playerNarrow = LoadTexture("pacman.png");
     Texture2D playerWide = LoadTexture("pacman2.png");
     Texture2D player = playerNarrow;
 
     while (!WindowShouldClose() && !IsKeyDown(KEY_ESCAPE)) {
-
-        if (frameCounter == frameRate / 4 ) {
+        if (frameCounter == frameRate / 4) {
             player = playerWide;
         }
-        if (frameCounter == frameRate / 2 ) {
+        if (frameCounter == frameRate / 2) {
             player = playerNarrow;
             frameCounter = 0;
         }
 
-        updatePosition(&x,&y);
+        updatePosition(&x, &y, &direction);
 
         BeginDrawing();
         ClearBackground(BLACK);
-        drawPacMan(player, x, y);
+        drawPacMan(player, x, y, direction);
         EndDrawing();
 
         frameCounter++;
     }
+
     UnloadTexture(playerNarrow);
     UnloadTexture(playerWide);
     CloseWindow();
@@ -49,21 +59,40 @@ int main(void) {
     return 0;
 }
 
-void drawPacMan(Texture2D player, int x, int y) {
-    DrawTextureV(player, (Vector2){x, y}, WHITE);
+void drawPacMan(Texture2D player, int x, int y, DIRECTION direction) {
+    Rectangle sourceRec = {0.0f, 0.0f, (float)player.width, (float)player.height};
+    Rectangle destRec = {x, y, (float)player.width, (float)player.height};
+    Vector2 origin = {(float)player.width / 2, (float)player.height / 2};
+
+    float rotation = 0.0f;
+    switch (direction) {
+        case RIGHT: rotation = 0.0f; break;
+        case LEFT:  rotation = 180.0f; break;
+        case UP:    rotation = -90.0f; break;
+        case DOWN:  rotation = 90.0f; break;
+        default: break;
+    }
+
+    DrawTexturePro(player, sourceRec, destRec, origin, rotation, WHITE);
 }
 
-void updatePosition(int *x, int *y) {
+void updatePosition(int *x, int *y, DIRECTION *direction) {
     if (IsKeyDown(KEY_UP)) {
         *y -= velocity_y;
+        *direction = UP;
     }
-    if (IsKeyDown(KEY_DOWN)) {
+    else if (IsKeyDown(KEY_DOWN)) {
         *y += velocity_y;
+        *direction = DOWN;
     }
-    if (IsKeyDown(KEY_RIGHT)) {
+    else if (IsKeyDown(KEY_RIGHT)) {
         *x += velocity_x;
+        *direction = RIGHT;
     }
-    if (IsKeyDown(KEY_LEFT)) {
+    else if (IsKeyDown(KEY_LEFT)) {
         *x -= velocity_x;
+        *direction = LEFT;
+    } else {
+        *direction = IDLE;
     }
 }
